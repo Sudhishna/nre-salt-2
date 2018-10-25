5989af944e93226459f5d76da8eca4721045f15f
Sudhishna <ssendhil@juniper.net>
2018-10-24 23:33:11 -0700

    first commit 2

--- images/vqfxspeedy/push.sh
+++ images/vqfxspeedy/push.sh
@@ -1,5 +1,3 @@
 docker build -f Dockerfile-snap1 -t antidotelabs/vqfxspeedy:snap1 .
-docker build -f Dockerfile-snap2 -t antidotelabs/vqfxspeedy:snap2 .
-docker build -f Dockerfile-snap3 -t antidotelabs/vqfxspeedy:snap3 .
 
 docker push antidotelabs/vqfxspeedy
